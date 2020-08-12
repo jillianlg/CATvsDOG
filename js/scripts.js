c42d6fec786a869324722afc2e43f7ced54b2b2d
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-12 09:34:59 -0700

    add img class, add append function

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
   $("button#cat").click(function() {
     $("ul#user").prepend("<li>Meow, Meow</li>");
+    $("#dog-img").hide();
+    $("#cat-img").show();
     $("ul#user").children("li").first().click(function() {
       $(this).remove();
     });
-    $("ul#webpage").children("li").first().click(function() {
-      $(this).remove();
-    });
+    // $("ul#webpage").children("li").first().click(function() {
+    //   $(this).remove();
+    // });
   });
+
 
 
   $("button#dog").click(function() {
     $("ul#user").prepend("<li>Woof, woof</li>");
-    $("ul#user").before("<li>Dog</li>");
+    $("#dog-img").show();
+    $("#cat-img").hide();
+    $("ul#user").append("click to clear selection");
+    $("ul#user").click(function() {
+      $(this).remove();
+    })
+
     $("ul#user").children("li").first().click(function() {
       $(this).remove();
-    });
-    $("ul#webpage").children("li").first().click(function() {
+    $("ul#user").siblings("li").first().click(function() {
       $(this).remove();
+    });
+    // $("ul#webpage").children("li").first().click(function() {
+    //   $(this).remove();
     });
   });
 });
